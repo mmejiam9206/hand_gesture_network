3dadc3e22373211f285184c3cff3aa137c080607
juanpabloduqueo <juanpabloduqueo@gmail.com>
2019-06-04 21:08:42 -0500

    agrega modelo lineal

--- model.py
+++ model.py
@@ -1,1 +1,9 @@
+import tensorflow as tf
+
+def getlinearmodel(num_class):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(32,32,3)))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+    return model
+
 
