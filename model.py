9c4a5af253358ed5e3605905cf3d77ba983b8376
juanpabloduqueo <juanpabloduqueo@gmail.com>
2019-06-12 17:12:26 -0500

    integracion de modelos

--- model.py
+++ model.py
@@ -1,54 +1,55 @@
-import tensorflow as tf
 
-def getlinearmodel(num_class):
+def get_linear_model(num_class):
     model = tf.keras.Sequential()
     model.add(tf.keras.layers.Flatten(input_shape=(32,32,3)))
     model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
     return model
 
-def get_alexnet(num_class_alexnet):
-    #Instantiate an empty model
+def get_one_layer_nn(num_class):
     model = tf.keras.Sequential()
-    # 1st Convolutional Layer
-    model.add(tf.keras.layers.Conv2D(filters=96, kernel_size=(5,5), activation='relu',padding='valid', input_shape=(32, 32, 3), strides = (1, 1)))
-    # Max Pooling
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides=(1,1), padding='valid'))
-    # 2nd Convolutional Layer
-    model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), padding='valid', activation = 'relu'))
-    # Max Pooling
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides=(1,1), padding='valid'))
-    # 3rd Convolutional Layer
-    model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid', activation = 'relu'))
-    # 4th Convolutional Layer
-    model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid', activation = 'relu'))
-    # 5th Convolutional Layer
-    model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid', activation = 'relu'))
-    # Max Pooling
-    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides=(1,1), padding='valid'))
-    # Passing it to a Fully Connected layer
+    model.add(tf.keras.layers.Flatten(input_shape=(32,32,3)))
+    model.add(tf.keras.layers.Dense(100, activation='relu'))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+    return model
+
+def get_mult_layer_nn(num_class):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(32,32,3)))
+    model.add(tf.keras.layers.Dense(100, activation='relu'))
+    model.add(tf.keras.layers.Dense(100, activation='relu'))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+    return model
+
+def get_lenet(num_class):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters=6, kernel_size=(5,5), activation='tanh',padding='same', input_shape=(32, 32, 3)))
+    model.add(tf.keras.layers.AveragePooling2D(pool_size=(2,2)))
+    model.add(tf.keras.layers.Conv2D(16, (5,5), activation='tanh'))
+    model.add(tf.keras.layers.AveragePooling2D((2,2)))
+    model.add(tf.keras.layers.Conv2D(120, (5,5), activation='tanh'))
     model.add(tf.keras.layers.Flatten())
-    # 1st Fully Connected Layer
-    #ESTA PRIMERA FC LA AGREGUÈ YO PORQUE CREO QUE FALTA
-    # 1st Fully Connected Layer
+    model.add(tf.keras.layers.Dense(84, activation='tanh'))
+    model.add(tf.keras.layers.Dense(num_class, activation='softmax'))
+    return model
+
+def get_alexnet(num_class):
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters=96, kernel_size=(5,5), activation='relu',padding='valid', input_shape=(32, 32, 3), strides = (1, 1)))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), padding='valid', activation = 'relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid', activation = 'relu'))
+    model.add(tf.keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid', activation = 'relu'))
+    model.add(tf.keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid', activation = 'relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(3,3), strides=(1,1), padding='valid'))
+    model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(9216, activation = 'relu'))
-    # Add Dropout to prevent overfitting
     model.add(tf.keras.layers.Dropout(0.4))
-    # 2nd Fully Connected Layer
     model.add(tf.keras.layers.Dense(4096, activation = 'relu'))
-    # Add Dropout
     model.add(tf.keras.layers.Dropout(0.4))
-    # 2nd Fully Connected Layer
     model.add(tf.keras.layers.Dense(4096, activation = 'relu'))
-    # Add Dropout
     model.add(tf.keras.layers.Dropout(0.4))
-    # 3rd Fully Connected Layer
     model.add(tf.keras.layers.Dense(1000, activation = 'relu'))
-    # Add Dropout
     model.add(tf.keras.layers.Dropout(0.4))
-    # Output Layer
-    model.add(tf.keras.layers.Dense(num_class_alexnet, activation = 'softmax'))
+    model.add(tf.keras.layers.Dense(num_class, activation = 'softmax'))
     return model 
-    #fin del modelo
-
-
-    #cambio prueba
